a0b1730c9dcfb32a9cfc9f44cd4dae181f5f8106
Deepak Patidar <mdipakpatidar@gmail.com>
2021-04-19 19:03:44 +0530

    error

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,15 +1,15 @@
 
 from django.urls import path
 from apps.dashboard import views
-app_name = "dashboard"
 
+app_name = "dashboard"
 urlpatterns = [
     path('home/',views.IndexView.as_view(),name='home'),
     path('employee/', views.CreateEmployeeView.as_view(), name='employee'),
     path('designation/', views.CreateDesignationView.as_view(), name='designation'),
     path('asset/', views.CreateAssetView.as_view(), name='asset'),
     
-    path('view_employee/', views.EmployeeView.as_view(), name='view_employee'),
+    path('view_employee/', views.EmployeeView.as_view(), name='employee-view'),
     path('view_designation/', views.DesignationView.as_view(), name='view_designation'),
 
     path('delete_employee/delete/<int:pk>',views.EmployeeDeleteView.as_view(),name='delete_employee'),
