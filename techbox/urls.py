6bcc04547e036e3cb6e8d185814c8e4767194f2a
Deepak Patidar <mdipakpatidar@gmail.com>
2021-05-06 16:49:11 +0530

    deployement

--- techbox/urls.py
+++ techbox/urls.py
@@ -20,16 +20,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
     path('', include('apps.authentication.urls')),
     path('', include('apps.pay.urls')),
     path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-
-    
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),   
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, 
 document_root=settings.STATIC_ROOT)
