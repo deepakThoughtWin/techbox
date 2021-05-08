804357c0410439f084ff01adf4bb8d4486492edc
Deepak Patidar <mdipakpatidar@gmail.com>
2021-05-08 18:14:30 +0530

    nothing

--- techbox/urls.py
+++ techbox/urls.py
@@ -1,17 +1,4 @@
-"""thetechbox URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
 from django.contrib import admin
@@ -27,11 +14,11 @@ urlpatterns = [
     path('', include('apps.authentication.urls')),
     path('', include('apps.pay.urls')),
     path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls')),   
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, 
+    urlpatterns += static(settings.STATIC_URL,
 document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, 
+    urlpatterns += static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
